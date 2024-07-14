6cd3310192ec28e950680578a6191a30bd6e3ebf
Daniel Ferrada <daniel.ferradabarria@gmail.com>
2024-07-14 15:53:39 -0400

    test Get pass

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -3,4 +3,12 @@ import { app } from '../index.js';
 
 describe("Operaciones CRUD de cafes", () => {
 
+    // prueba para ruta GET
+    it('should return a status code 200 and an array with at least 1 object', async () =>{
+        const resp = await request(app).get('/cafes');
+        expect(resp.status).toBe(200);
+        expect(Array.isArray(resp.body)).toBe(true);
+        expect(resp.body.length).toBeGreaterThan(0);
+    });
+
 });
